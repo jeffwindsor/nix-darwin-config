b2abbdb0af2211116e5a953d193a0e1c8b88e4bb
Jeff Windsor <jeff.windsor@gmail.com>
2025-03-30 17:39:35 -0700

    ADD Megasync - no enable flag

--- packages/default.nix
+++ packages/default.nix
@@ -31,6 +31,11 @@
       zsh.enable = true;
     };
 
+    # not yet enable-afied
+    environment.systemPackages = with pkgs; [
+      megasync
+    ];
+    
     # fonts (not converted to separate nix files for now)
     fonts.packages = with pkgs; [
       nerd-fonts.jetbrains-mono
