03d4d52f62523b68b2333e38ec8fa9ff08939e2f
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-04 15:28:59 -0700

    Make brew shell take in command detault to install

--- brews.sh
+++ brews.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+brew_command="${1:-install}"
+
 # install brew if missing
 if ! command -v brew &> /dev/null; then
   /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # install apps
-brew reinstall --cask jordanbaird-ice  # bar icon manager
-brew reinstall --cask keepingyouawake  # like caffiene
-brew reinstall --cask zen-browser      # browser not in nix package store as of 2025-03-21
-brew reinstall --cask ghostty          # currently broken package in nix as of 2025-03-17
-brew reinstall --cask balenaetcher     # usb iso app 
+brew $brew_command --cask jordanbaird-ice  # bar icon manager
+brew $brew_command --cask keepingyouawake  # like caffiene
+brew $brew_command --cask zen-browser      # browser not in nix package store as of 2025-03-21
+brew $brew_command --cask ghostty          # currently broken package in nix as of 2025-03-17
+brew $brew_command --cask balenaetcher     # usb iso app 
 
