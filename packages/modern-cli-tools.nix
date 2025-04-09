a2d27371f40225f73df1b82e215373b548e9437a
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-09 12:16:50 -0700

    REMOVE uutils
    These are not quite ready for prime time

--- packages/modern-cli-tools.nix
+++ packages/modern-cli-tools.nix
@@ -1,5 +1,4 @@
 { config, lib, pkgs, ... }:{
-  
 
   options.packages.modern-cli-tools.enable = lib.mkEnableOption "modern-cli-tools";
 
@@ -12,8 +11,8 @@
       fzf  # fuzzy finder
       ripgrep  #grep replacement
       sd  # sed replacement
-      starship # prompt
-      uutils-coreutils-noprefix # rust replacement for GNU tools
+      starship  # prompt
+      # uutils-coreutils-noprefix # rust replacement for GNU tools
       yazi  # tui file manager
     ];
     
