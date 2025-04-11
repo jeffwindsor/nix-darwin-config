f05c877e0961eec6e0dec66f6db176cfc5e59af2
Windsor, Jeff <jwindsor@cj.com>
2025-04-11 14:32:22 -0700

    Fix mono-fonts content to match filename

--- packages/mono-fonts.nix
+++ packages/mono-fonts.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:{
-  options.packages.fonts.enable = lib.mkEnableOption "mono-fonts";
+  options.packages.mono-fonts.enable = lib.mkEnableOption "mono-fonts";
 
-  config = lib.mkIf config.packages.fonts.enable {
+  config = lib.mkIf config.packages.mono-fonts.enable {
     fonts.packages = with pkgs; [
       nerd-fonts.jetbrains-mono
       atkinson-hyperlegible-mono
