9fa5148178d9d9394aec3678314b7374912ba260
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-11 16:50:58 -0700

    ADD nyxt browser

---
+++ packages/nyxt.nix
@@ -0,0 +1,7 @@
+{ config, lib, pkgs, ... }:{
+  options.packages.nyxt.enable = lib.mkEnableOption "nyxt";
+
+  config = lib.mkIf config.packages.nyxt.enable {
+    environment.shells = with pkgs; [ nyxt ];
+  };
+}
