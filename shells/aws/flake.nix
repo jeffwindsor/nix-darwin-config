c15095f90d13cbfc7e21b54ca6a690a80a4d5cba
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-11 19:12:32 -0700

    prettify

--- shells/aws/flake.nix
+++ shells/aws/flake.nix
@@ -9,18 +9,19 @@
     in
     {
       devShell = pkgs.mkShell {
-        	name = "AWS";
+      	name = "AWS";
         packages = with pkgs; [
-awscli2
-nodePackages.aws-cdk
-];
-          shellHook = ''
-            echo -e "\e[1;94m == Development Environment =="
-            # add --version or some other call to list dev packages
-            echo -e "https://operations.cj.dev/"
-            aws --version
-            echo -e "\e[0m"
-          '';
+          awscli2
+          nodePackages.aws-cdk
+        ];
+        shellHook = ''
+          echo -e "\e[1;94m == Development Environment =="
+          # add --version or some other call to list dev packages
+          echo -e "https://operations.cj.dev/"
+          aws --version
+          cdk --version
+          echo -e "\e[0m"
+        '';
       };
     }
   );
