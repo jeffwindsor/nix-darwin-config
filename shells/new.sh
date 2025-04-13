e4e6c264c3c5e5f152f42debecf73d23788c4265
Jeff Windsor <jeff.windsor@gmail.com>
2025-04-13 15:06:48 -0700

    New script convert to run from anywhere

--- shells/new.sh
+++ shells/new.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
-
 shell_name="$1"
-package_version="${2:-nixpkgs-24.11-darwin}"
-path="$(dirname "$0")/$shell_name"
 
 # create sub directory
-mkdir -p "$path"
+mkdir -p "./$shell_name"
 
 # read template.nix
 # 1. replace placeholders with values
 # 2. remove all comment lines
 # 3. put result in the new directory as flake.nix
-cat shell-flake-template.nix | sed \
+cat "$(dirname "$0")/shell-flake-template.nix" | sed \
 	-e "s/{shell_name}/$shell_name/g" \
-	-e "s/{package_version}/$package_version/g" \
 	-e '/^ *#/d' \
-	>"$path/flake.nix"
+	>"./$shell_name/flake.nix"
